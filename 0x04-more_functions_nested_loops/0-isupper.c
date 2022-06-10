466e58c0a97fdd10f2b7f0cfb381cbe36f65bd34
hnghiikumbu <hnghiikumbu@gmail.com>
2022-06-10 15:25:08 -0700

    late work

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - chek for upper case letter
+ * @c : character to check the case
+ * Retrn : 0 or 1 
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}

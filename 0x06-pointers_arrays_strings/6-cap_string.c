7191bdeaa786815c99731182bc64e33abea7241b
hnghiikumbu <hnghiikumbu@gmail.com>
2022-06-17 06:35:03 -0700

    capitalise

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,6 +16,7 @@ char *cap_string(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		flag = 0;
+
 		if (i == 0)
 		{
 			flag = 1;
@@ -24,19 +25,19 @@ char *cap_string(char *str)
 		{
 			for (ii = 0; sep[ii] != '\0'; ii++)
 			{
-															if (str[i - 1] == sep[ii])
-															{
+														if (str[i - 1] == sep[ii])
+				{
 					flag = 1;
 					break;
-															}
-														}
+				}
+			}
 		}
 		if (flag == 1)
 		{
 			if (str[i] <= 'z' && str[i] >= 'a')
 			{
 				str[i] -= ('a' - 'A');
-														}
+			}
 		}
 
 	}

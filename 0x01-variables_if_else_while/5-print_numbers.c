a209883959fc2096110a26e3db157d3deef2e16b
Elalia Nghiikumbu <hnghiikumbu@gmail.com>
2022-12-12 16:31:50 -0800

    number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/**i
  * main -> assign a random number to the variable n each time it is executed 
  * and print the last digit of the number stored in the variable n
  * Return: Always 0 (success)

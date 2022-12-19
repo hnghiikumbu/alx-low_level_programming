596946d3e84a1b547cb5501ae2f958175d55aeef
Elalia Nghiikumbu <hnghiikumbu@gmail.com>
2022-12-19 15:38:08 -0800

    revrse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,7 +16,8 @@ void rev_string(char *s)
 
 		;
 	/*swap the string by looping to half the string*/
-	for (i = 0; i < l / 2; ++i)							{
+	for (i = 0; i < l / 2; ++i)							
+	{
 		ch = s[i];
 		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
 												s[l - 1 - i] = ch;

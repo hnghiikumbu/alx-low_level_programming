2630e75efc13c1930be9d3b8deadbb3083d93376
Elalia Nghiikumbu <hnghiikumbu@gmail.com>
2022-12-20 18:19:37 -0800

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 /**
- *
  * _strcpy - copies a string from one pointer to another
  *
  *	including the terminating null byte (\0)
@@ -12,11 +11,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int a, b = 0;
-	
+	int a, b;
+
+	b = 0;	
 	for (a = 0; src[a] != '\0'; ++a)
 	{
-		dest[b] = src[a];								++b;
+		dest[b] = src[a];
+		++b;
 	}
 	dest[b] = '\0';
 

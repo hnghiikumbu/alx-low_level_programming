9d05da67a327123fa7fccb24c462e56d4aad8cc3
Elalia Nghiikumbu <hnghiikumbu@gmail.com>
2023-01-21 12:14:58 -0800

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -23,9 +23,7 @@ char *create_array(unsigned int size, char c)
 	array = (char *)malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
-	
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	
 	return (array);
 }
